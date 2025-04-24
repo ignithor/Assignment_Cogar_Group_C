38bcc2918df9b0a4f1a3b94310bd588e09020499
GitBB3 <beaujean.bertille@orange.fr>
2025-04-24 19:27:45 +0200

    fix: Try to fix the documentation of GitHub.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
 project = 'Assignment_Cogar_Group_C'
 copyright = '2025, BEAUJEAN Bertille PHAM DANG Paul ROYANT Emma'
 author = 'BEAUJEAN Bertille PHAM DANG Paul ROYANT Emma'
