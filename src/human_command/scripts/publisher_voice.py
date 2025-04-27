0ab9c40a2fded948732ae2b21f7346aab296b318
Paul P <paul.phamdang@gmail.com>
2025-04-27 22:39:54 +0200

    Update super launch

---
+++ src/human_command/scripts/publisher_voice.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def carrot_publisher():
+    # Initialize the ROS node
+    rospy.init_node('carrot_publisher', anonymous=True)
+    
+    # Create a publisher that publishes to the 'carrot_topic'
+    pub = rospy.Publisher('/voice_command', String, queue_size=10)
+    
+    # Set the publishing rate to 0.5 Hz (i.e., every 2 seconds)
+    rate = rospy.Rate(0.5)
+    
+    while not rospy.is_shutdown():
+        message = "cut carrots"
+        rospy.loginfo(f"Publishing: {message}")
+        pub.publish(message)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        carrot_publisher()
+    except rospy.ROSInterruptException:
+        pass
